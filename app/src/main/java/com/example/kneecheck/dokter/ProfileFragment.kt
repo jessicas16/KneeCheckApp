ab1fdfe2b5b4a593c5e1e4f578b639432ac3af4f
ReyKeyS <104489667+ReyKeyS@users.noreply.github.com>
2024-12-03 21:47:09 +0700

    Fix ProfileFrag + BISA LOGOUT

--- app/src/main/java/com/example/kneecheck/dokter/ProfileFragment.kt
+++ app/src/main/java/com/example/kneecheck/dokter/ProfileFragment.kt
@@ -1,8 +1,13 @@
+package com.example.kneecheck.dokter
+
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.kneecheck.LoginActivity
 import com.example.kneecheck.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -20,6 +25,12 @@ class ProfileFragment : Fragment() {
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        binding.imgButtonLogOutDokter.setOnClickListener {
+            val intent = Intent(requireActivity(), LoginActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
 
         return root
     }
