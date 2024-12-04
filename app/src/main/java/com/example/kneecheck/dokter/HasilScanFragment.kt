a1f2a8efeebc2dfb05ac55d9dc9eb0d0748d56e8
dwisukmaayu <dwisukm4ayu20@gmail.com>
2024-12-04 17:26:27 +0700

    pop up

--- app/src/main/java/com/example/kneecheck/dokter/HasilScanFragment.kt
+++ app/src/main/java/com/example/kneecheck/dokter/HasilScanFragment.kt
@@ -1,9 +1,12 @@
 package com.example.kneecheck.dokter
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.kneecheck.R
@@ -35,4 +38,34 @@ class HasilScanPasienFragment : Fragment() {
         binding.severityText.text = "Confidence: $confidenceScore"
         binding.tipsText.text = "Pengobatan: $pengobatan"
     }
+
+    private fun showConfirmationDialog() {
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.layout_popup, null)
+        val dialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .setCancelable(false) // Dialog tidak bisa ditutup dengan klik di luar
+            .create()
+
+        // Klik tombol "Belum"
+        val buttonBelum = dialogView.findViewById<Button>(R.id.btnBelum)
+        buttonBelum.setOnClickListener {
+            dialog.dismiss()
+            // Arahkan ke halaman Belum Punya Akun
+            val intent = Intent(this, BelumPunyaAkunFragment::class.java)
+            startActivity(intent)
+        }
+
+        // Klik tombol "Sudah"
+        val buttonSudah = dialogView.findViewById<Button>(R.id.btnSudah)
+        buttonSudah.setOnClickListener {
+            dialog.dismiss()
+            // Arahkan ke halaman Sudah Punya Akun
+            val intent = Intent(this, SudahPunyaAkunFragment::class.java)
+            startActivity(intent)
+        }
+
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent) // Background transparan
+        dialog.show()
+    }
+
 }
